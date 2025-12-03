6afdfe6f853cc3285d98cb944b91cb605ad3ac24
Jednaczwarta <mikox5720@gmail.com>
2025-12-03 14:00:59 +0100

    Tworzenie funkcjonalności

--- app/src/main/java/com/example/zadaniezaliczenie/MainActivity.java
+++ app/src/main/java/com/example/zadaniezaliczenie/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.zadaniezaliczenie;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -32,5 +37,33 @@ public class MainActivity extends AppCompatActivity {
         );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        EditText name = findViewById(R.id.nameET);
+        EditText surname = findViewById(R.id.surnameET);
+        EditText quantity = findViewById(R.id.quantityET);
+        CheckBox smallAndCapital = findViewById(R.id.smallAndCapitalCB);
+        CheckBox numbers = findViewById(R.id.numbersCB);
+        CheckBox specialLetters = findViewById(R.id.specialLettersCB);
+        Button generatePass = findViewById(R.id.generatePassBTN);
+        Button commit = findViewById(R.id.commitBTN);
+
+        int quantityInt = Integer.parseInt(quantity.getText().toString());
+
+        StringBuilder allowedCharacters = new StringBuilder();
+        allowedCharacters.append("abcdefghijklmnopqrstuwvxyz");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        generatePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (quantityInt < 4) {
+                    builder.setMessage("Hasło jest za krótkie").setTitle("Błąd");
+                    AlertDialog error = builder.create();
+                }else {
+
+                }
+            }
+        });
     }
 }
